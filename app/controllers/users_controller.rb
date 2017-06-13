caaa61b2e11c26f27f33cffb504bdb646156e839
Mike Tarkington <tarkmike@gmail.com>
2017-06-13 09:54:45 -0700

    move navbar inline style to css file and prevent users from registering with blankpw

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,17 @@ get '/users/new' do
 end
 
 post '/users' do
-  user = User.new(params[:user])
-  if user.save
-    login(user)
-    redirect '/'
-  else
-    @errors = user.errors.full_messages
+  if params[:user][:password] == ""
+    @errors = ["Password cannot be blank."]
     erb :'users/new'
+  else
+    user = User.new(params[:user])
+    if user.save
+      login(user)
+      redirect '/'
+    else
+      @errors = user.errors.full_messages
+      erb :'users/new'
+    end
   end
 end
